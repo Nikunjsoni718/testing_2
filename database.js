e6b8eba22d1d65e2d410dd7a6b62dc3f55318d8d
Nikunjsoni718 <sanjusoni15699@gmail.com>
2026-09-05 16:32:12 +0530

    ad db password

--- database.js
+++ database.js
@@ -2,7 +2,8 @@
 const dbConfig = {
     host: "localhost",
     port: 5432,
-    username: "admin_user"
+    username: "admin_user",
+    password: "super_secret_password_123" // DO NOT DO THIS IN PRODUCTION
 };
 
 export default dbConfig;
